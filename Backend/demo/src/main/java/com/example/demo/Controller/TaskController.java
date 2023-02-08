03021bcc9a1b8007dc6e841b3dadfe7fc7ddd016
ahmedbanna212 <ahmedbanna212@gmail.com>
2023-02-08 10:25:55 +0200

    updates

--- Backend/demo/src/main/java/com/example/demo/Controller/TaskController.java
+++ Backend/demo/src/main/java/com/example/demo/Controller/TaskController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,21 +25,25 @@ import com.example.demo.Service.TaskService;
 public class TaskController {
     @Autowired
     private TaskService taskService;
-    
+
+    @CrossOrigin(origins = "*", maxAge = 3600)
     @GetMapping("/getTask")
     public ResponseEntity<List<TaskDTO>> task() {
         return new ResponseEntity<>(taskService.getAllTasksAndStates(), HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "*", maxAge = 3600)
     @PostMapping("/addTask")
     public ResponseEntity<?> addTask(@RequestBody Task task) {
         return new ResponseEntity<>(taskService.addTask(task), HttpStatus.OK);
         
     }
+    @CrossOrigin(origins = "*", maxAge = 3600)
     @DeleteMapping("/deleteTask/{id}")
     public ResponseEntity<?> deleteTask(@PathVariable  Long id) {
         return new ResponseEntity<>(taskService.deleteTask(id ), HttpStatus.OK);
     }
+    @CrossOrigin(origins = "*", maxAge = 3600)
     @PutMapping("/updateTask/{id}")
     public ResponseEntity<?> updateTask(@PathVariable  Long id,@RequestBody TaskDTO task) {
         return new ResponseEntity<>(taskService.updateTask(id,task), HttpStatus.OK);
